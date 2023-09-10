35bf7b504713f8bdfc113407206db104e023da98
Alessio <a.chiffi@ry.com>
2023-09-10 16:03:20 +0100

    fix example select to show updated options - refactored and added prettier

--- src/js/index.js
+++ src/js/index.js
@@ -1,78 +1,84 @@
-import { magicMouse } from "./magicmouse";
-import '../scss/website.scss'
-import { createApp } from 'vue/dist/vue.esm-bundler'
+import { magicMouse } from './magicmouse';
+import '../scss/website.scss';
+import { createApp } from 'vue/dist/vue.esm-bundler';
 
 const app = createApp({
-    data() {
-        return {
-            selectedEffect: 'example #1',
-            effectList: [
-              "example #1",
-              "example #2",
-              "example #3",
-              "example #4",
-              "example #5",
-            ],
-            options: {
-              outerStyle: "circle",
-              hoverEffect: "circle-move",
-              hoverItemMove: false,
-              defaultCursor: false,
-              outerWidth: 30,
-              outerHeight: 30,              
-            }
-        }
-    },
-    mounted() {
-        magicMouse(this.options);
-        // prettier();
-
-    },
-    methods: {
-        changeEffect() {
-          document.body.style.cursor = "unset";
-          var cursorDOM = document.getElementById("magicMouseCursor");
-          var pointerDOM = document.getElementById("magicPointer");
-          if (cursorDOM) {
-            cursorDOM.parentNode.removeChild(cursorDOM)
-          }
-          if (pointerDOM) {
-            pointerDOM.parentNode.removeChild(pointerDOM)
-          }
-          switch (this.selectedEffect) {
-            case "example #1":
-              this.options.hoverEffect = "circle-move"
-              this.options.outerStyle = "circle-basic"
-              break;
-            case "example #2":
-              this.options.hoverEffect = "pointer-overlay"
-              this.options.outerStyle = "disable"
-              break;
-            case "example #3":
-              this.options.hoverEffect = "pointer-blur"
-              this.options.outerStyle = "disable"
-              break;
-            case "example #4":
-              this.options.outerWidth = 41;
-              this.options.outerHeight = 41;
-              this.options.hoverEffect = "pointer-blur"
-              this.options.outerStyle = "circle-basic"
-              break;
-            case "example #5":
-              this.options.outerWidth = 41;
-              this.options.outerHeight = 41;
-              this.options.hoverEffect = "circle-move"
-              this.options.outerStyle = "circle-basic"
-              this.options.defaultCursor = true
-              break;
-          }
-          magicMouse(this.options);
-
-          let codeBlock = document.getElementById("codeSection")
-          codeBlock.classList.remove("prettyprinted")
-          codeBlock.innerHTML = "options = " + JSON.stringify(this.options, null, 2) + "; \n magicMouse(options);";          
-        }
+  data() {
+    return {
+      selectedEffect: 'example #1',
+      effectList: ['example #1', 'example #2', 'example #3', 'example #4', 'example #5'],
+      options: {
+        outerStyle: 'circle',
+        hoverEffect: 'circle-move',
+        hoverItemMove: false,
+        defaultCursor: false,
+        outerWidth: 30,
+        outerHeight: 30,
       },
-})
+    };
+  },
+  mounted() {
+    magicMouse(this.options);
+  },
+  methods: {
+    setSelectedEffect() {
+      switch (this.selectedEffect) {
+        case 'example #1':
+          this.options.hoverEffect = 'circle-move';
+          this.options.outerStyle = 'circle-basic';
+          break;
+        case 'example #2':
+          this.options.hoverEffect = 'pointer-overlay';
+          this.options.outerStyle = 'disable';
+          break;
+        case 'example #3':
+          this.options.hoverEffect = 'pointer-blur';
+          this.options.outerStyle = 'disable';
+          break;
+        case 'example #4':
+          this.options.outerWidth = 41;
+          this.options.outerHeight = 41;
+          this.options.hoverEffect = 'pointer-blur';
+          this.options.outerStyle = 'circle-basic';
+          break;
+        case 'example #5':
+          this.options.outerWidth = 41;
+          this.options.outerHeight = 41;
+          this.options.hoverEffect = 'circle-move';
+          this.options.outerStyle = 'circle-basic';
+          this.options.defaultCursor = true;
+          break;
+      }
+    },
+    printOptions() {
+      let codeBlock = document.getElementById('codeSection');
+      codeBlock.classList.remove('prettyprinted');
+      codeBlock.innerHTML =
+        'options = ' + JSON.stringify(this.options, null, 2) + '\nmagicMouse(options);';
+    },
+    changeEffect() {
+      document.body.style.cursor = 'unset';
+      var cursorDOM = document.getElementById('magicMouseCursor');
+      var pointerDOM = document.getElementById('magicPointer');
+      if (cursorDOM) {
+        cursorDOM.parentNode.removeChild(cursorDOM);
+      }
+      if (pointerDOM) {
+        pointerDOM.parentNode.removeChild(pointerDOM);
+      }
+      this.setSelectedEffect(this.selectedEffect);
+      this.printOptions();
+      magicMouse(this.options);
+    },
+  },
+  watch: {
+    selectedEffect() {
+      this.setSelectedEffect();
+      this.$nextTick(() => {
+        this.printOptions();
+      });
+    },
+  },
+});
 
-app.mount('#app')
+app.mount('#app');
